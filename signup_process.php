9bc556ac0d6d8e7088c0c62e721c6f5656011a9e
sanuriyp <55776117+sanuriyp@users.noreply.github.com>
2020-05-21 09:56:01 +0530

    Create signup_process.php

---
+++ signup_process.php
@@ -0,0 +1,92 @@
+<?php
+session_start();
+include("db.php");
+
+	$pagename="Your Sign Up Results"; //Create and populate a variable called $pagename
+	echo "<link rel=stylesheet type=text/css href=mystylesheet.css>"; //Call in stylesheet
+	echo "<title>".$pagename."</title>"; //display name of the page as window title
+	echo "<body>";
+	include ("headfile.html"); //include header layout file
+	echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
+	
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$address = $_POST['address'];
+	$pcode = $_POST['pcode'];
+	$telno = $_POST['telno'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$confirmPassword = $_POST['confirmPassword'];
+	
+			
+		//To check if the fields are filled.
+		if (!empty($fname)&&($lname)&&($address)&&($pcode)&&($telno)&&($email)&&($password)) {
+			
+			$validatePaswsword = strcmp($password,$confirmPassword);
+			
+			//validating the password
+			if($validatePaswsword == 0){
+				
+				//validating the email
+				if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+					
+					$SQL="insert into 
+						users (userFName, userSName, userAddress, userPostCode,	userTelNo, userEmail, userPassword)
+						values ('".$fname."', '".$lname."','".$address."','".$pcode."', '".$telno."','".$email."','".$password."')";
+					
+					$exeSQL = mysqli_query($conn, $SQL) or die (mysqli_error($conn));
+					
+					//to check mysql errors
+					if (mysqli_errno($conn)==0)
+					{
+						echo "<b>Sign-Up Successful! </b><br><br>";
+						echo "You have sucessfully registered to our page. <br><br>";
+						echo "To continue, please<a href='login.php'> LOGIN </a> ";
+					}else{
+						echo "<p>There is an error with the details you entered.</p>";
+						
+						//To check if the email exists in the database
+						if (mysqli_errno($conn)==1062)
+						{
+							echo "<b>Sign-Up Failed</b><br><br>";
+							echo "Email already in use .<br>";
+							echo "You may be already registered or try another email address.<br><br>";
+							echo "Go back to <a href='signup.php'>SIGN UP</a>";
+						}
+						if (mysqli_errno($conn)==1064)
+						{
+							echo "<b>Sign-Up Failed</b><br><br>";
+							echo "<br>Invalid characters entered in the form.";
+							echo "Make sure you avoid the following characters : apostrophes like ['] and backslahes like [\] <br><br>";
+							echo "Go back to <a href='signup.php'>SIGN UP</a>";
+						}
+					}
+					
+				}else{
+					echo "<b>Sign-Up Failed</b><br><br>";
+					echo "Email not valid<br> ";
+					echo "Make sure you enter a correct email address<br><br>";
+					echo "Go back to <a href='signup.php'>SIGN UP</a> again.";
+				}
+				
+				
+			}else{
+				echo "<b>Sign-Up Failed</b><br><br>";
+				echo "The 2 Passwords are not matching<br>";
+				echo "Make sure you ener them correctly<br><br>";
+				echo "Go back to <a href='signup.php'>SIGN UP</a>";
+				
+			}
+
+		}else{
+			echo "<b>Sign-Up Failed</b><br><br>";
+			echo"<p>Your SignUp form is incompleted and all fields are mandatory<p>";
+			echo"Make sure you provide all the required details<br><br>";
+			echo"Go back <a href='signup.php'>SIGN UP</a> ";
+		}
+	//echo"Hi $fname";
+	
+	
+	include("footfile.html"); //include head layout
+	echo "</body>";
+?>
